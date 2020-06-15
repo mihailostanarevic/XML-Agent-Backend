f154813101b87dae6138a04726df337ce8147452
jelenadragisic <lela.jelena@gmail.com>
2020-06-16 00:34:00 +0200

    reserve ad by agent

---
+++ agent-backend/src/main/java/com/rentacar/agentbackend/dto/response/RequestResponse.java
@@ -0,0 +1,14 @@
+package com.rentacar.agentbackend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestResponse {
+
+    private String message;
+
+}
