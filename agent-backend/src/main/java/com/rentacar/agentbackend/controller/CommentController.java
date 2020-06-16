a2383a9203b04b242c554f4cd7fc3fa9ec25b174
dimitrije97 <dimitrijesalic@yahoo.com>
2020-06-16 14:20:57 +0200

    Approve comment request

--- agent-backend/src/main/java/com/rentacar/agentbackend/controller/CommentController.java
+++ agent-backend/src/main/java/com/rentacar/agentbackend/controller/CommentController.java
@@ -3,10 +3,9 @@ package com.rentacar.agentbackend.controller;
 import com.rentacar.agentbackend.dto.request.CommentAdRequest;
 import com.rentacar.agentbackend.dto.response.CommentResponse;
 import com.rentacar.agentbackend.service.ICommentService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/comments")
@@ -21,5 +20,10 @@ public class CommentController {
     @PostMapping
     CommentResponse commentAd(@RequestBody CommentAdRequest request) throws Exception {
         return _commentService.commentAd(request);
+    }
+
+    @PutMapping("approve/{id}/comment")
+    void approveComment(@PathVariable UUID id) throws Exception {
+        _commentService.approveComment(id);
     }
 }
--- agent-backend/src/main/java/com/rentacar/agentbackend/service/ICommentService.java
+++ agent-backend/src/main/java/com/rentacar/agentbackend/service/ICommentService.java
@@ -3,7 +3,11 @@ package com.rentacar.agentbackend.service;
 import com.rentacar.agentbackend.dto.request.CommentAdRequest;
 import com.rentacar.agentbackend.dto.response.CommentResponse;
 
+import java.util.UUID;
+
 public interface ICommentService {
 
     CommentResponse commentAd(CommentAdRequest request) throws Exception;
+
+    void approveComment(UUID id) throws Exception;
 }
